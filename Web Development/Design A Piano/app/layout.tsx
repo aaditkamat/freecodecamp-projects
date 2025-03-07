565900f53fb8bc9037d333c81fc3bac885e6f914
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2025-03-06 23:16:16 -0500

    Update Design A Piano style

--- Web Development/Design A Piano/app/layout.tsx
+++ Web Development/Design A Piano/app/layout.tsx
@@ -14,7 +14,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-green-950">{children}</body>
+      <body className="w-screen h-screen">
+        <div className="h-1/4 invisible" />
+        <div className="flex flex-row">
+          <div className="w-1/3 invisible" />
+          {children}
+          <div className="w-1/3 invisible" />
+        </div>
+        <div className="h-1/3 invisible" />
+      </body>
     </html>
   );
 }
--- Web Development/Design A Piano/app/page.tsx
+++ Web Development/Design A Piano/app/page.tsx
@@ -4,10 +4,15 @@ import Piano from "../components/Piano";
 
 export default function Home() {
   return (
-    <Fragment>
+    <div className="bg-green-950 w-fit rounded-lg">
+      <div className="h-2 invisible"></div>
       <Logo />
       <div className="h-12 invisible"></div>
-      <Piano />
-    </Fragment>
+      <div className="flex flex-row">
+        <Piano />
+        <div className="h-[200] w-8 invisible"></div>
+      </div>
+      <div className="h-12 invisible"></div>
+    </div>
   );
 }
